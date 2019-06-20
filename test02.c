42e3d41f673c19a56086e244b22e06f495189c36
emsi-iggy <michaelcoldpl@gmail.com>
2019-06-20 18:24:08 +0200

    Test2.1

--- test02.c
+++ test02.c
@@ -16,7 +16,8 @@ int nowyZegarLamporta = 0;
 int ileZajecyPozostalo = 50;
 int tagWiadomosci = 1; //1-licencja, 2-zajace, 3-transport
 
-int iloscZgod[3] = 0;
+int Licencje = 2;
+int Mysliwi = 4;
 
 MPI_Status status;
 
@@ -29,40 +30,75 @@ int rozmiarWiadomosci = sizeof(wiadomosc);
 wiadomosc[0] to idProcesu
 wiadomosc[1] to zegarLamporta
 wiadomosc[2]= {1,2,3} to pytam o licencje,zające,transporty
-wiadomosc[3]= {} to idProcesu
+wiadomosc[3]= {1} to czy zgoda (-1 to nie zgoda)
 wiadomosc[4] to ile zajęcy chce zabić
 *          ********/
 int ileMamZgodLicencja = 0;
 int ileMamZgodZajace = 0;
 int ileMamZgodTranport = 0;
+int procesChceLicencje = 0;
+
+void aktualizujZegar();
 
 void * funkcjaWatku() {
 	while(!end) {
 		MPI_Recv(&wiadomosc, rozmiarWiadomosci, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		printf("status:%d, Otrzymalem od procesu: %d, wiadomosc o zegarze[%d], coChce[%d]\n",status.MPI_SOURCE,wiadomosc[0],wiadomosc[1],wiadomosc[2]);
-		//if(nowyZegarLamporta < zegarLamporta) = max()
-		nowyZegarLamporta = max(zegarLamporta, wiadomosc[1]);
-		if(wiadomosc[2]==1) {
-			if(zegarLamporta > wiadomosc[1]) { //proces pyta wczesniej niz ja 
-				wiadomosc[3] = 1;
-				MPI_Send(wiadomosc,rozmiarWiadomosci, MPI_INT, wiadomosc[0], status.MPI_SOURCE, MPI_COMM_WORLD);
-			}
-			else if(zegarLamporta == wiadomosc[1]) {
-				if(rank > wiadomosc[0]) {
+		printf("WATEK >> status:%d, Otrzymalem od procesu: %d, wiadomosc o zegarze[%d], coChce[%d], czyDalemZgode=%d\n",status.MPI_SOURCE,wiadomosc[0],wiadomosc[1],wiadomosc[2],wiadomosc[3]);
+		zegarLamporta = max(zegarLamporta, wiadomosc[1]);
+		
+		if(wiadomosc[3]==1) {
+			ileMamZgodLicencja += 1;
+		}
+		else if(wiadomosc[3]==2) {
+			ileMamZgodZajace += 1;
+		}
+		else if(wiadomosc[3]==3) {
+			ileMamZgodTranport += 1;
+		}
+		else {
+			int doKogo = -1;
+			if(wiadomosc[2]==1) { //byla prosba o licencje
+				if(!procesChceLicencje) {
+					aktualizujZegar();
+
+					doKogo = wiadomosc[0];
+					wiadomosc[0] = rank;
+					wiadomosc[1] = zegarLamporta;
 					wiadomosc[3] = 1;
-					MPI_Send(wiadomosc,rozmiarWiadomosci, MPI_INT, wiadomosc[0], status.MPI_SOURCE, MPI_COMM_WORLD);
+					MPI_Send(wiadomosc,rozmiarWiadomosci, MPI_INT, doKogo, status.MPI_SOURCE, MPI_COMM_WORLD);
+
 				}
 				else {
-					wiadomosc[3] = 0;
-					MPI_Send(wiadomosc,rozmiarWiadomosci, MPI_INT, wiadomosc[0], status.MPI_SOURCE, MPI_COMM_WORLD);
+					if(staryZegarLamporta > wiadomosc[1]) { //proces pyta wczesniej niz ja 
+						aktualizujZegar();
+
+						doKogo = wiadomosc[0];
+						wiadomosc[0] = rank;
+						wiadomosc[1] = zegarLamporta;
+						wiadomosc[3] = 1;
+						MPI_Send(wiadomosc,rozmiarWiadomosci, MPI_INT, doKogo, status.MPI_SOURCE, MPI_COMM_WORLD);
+					}
+					else if(zegarLamporta == wiadomosc[1]) {
+						if(rank > wiadomosc[0]) {
+							aktualizujZegar();
+
+							doKogo = wiadomosc[0];
+							wiadomosc[0] = rank;
+							wiadomosc[1] = zegarLamporta;
+							wiadomosc[3] = 1;
+							MPI_Send(wiadomosc,rozmiarWiadomosci, MPI_INT, doKogo, status.MPI_SOURCE, MPI_COMM_WORLD);
+						}
+					}
+					else {
+						//wiadomosc[3] = 0;
+					//	iadomosc,rozmiarWiadomosci, MPI_INT, wiadomosc[0], status.MPI_SOURCE, MPI_COMM_WORLD);
+					}
+				
 				}
+				
 			}
-			else {
-				wiadomosc[3] = 0;
-				iadomosc,rozmiarWiadomosci, MPI_INT, wiadomosc[0], status.MPI_SOURCE, MPI_COMM_WORLD);
-			}
-			
 		}
+		
 	}
 	return NULL;
 }
@@ -85,10 +121,11 @@ int main(int argc, char **argv) {
     /*
         chce licencje:
     */
-    if ((rand() % 101 + 1) > 50) { //50% szans ze chce licencje, jezeli chce to:
-		pthread_mutex_lock(&mutexZegar);
-        zegarLamporta += 1;
-        pthread_mutex_unlock(&mutexZegar);
+	procesChceLicencje = (rand() % 101 + 1) > 50;
+    if (procesChceLicencje) { //50% szans ze chce licencje, jezeli chce to:
+		printf("Proces: %d chce dostac licencje!\n",rank);
+		
+		aktualizujZegar();
 
 		staryZegarLamporta = zegarLamporta;
 
@@ -100,14 +137,25 @@ int main(int argc, char **argv) {
 		for(int i=0; i<size; i++) {
 			if(i==rank) continue;
 			MPI_Send(wiadomosc,rozmiarWiadomosci, MPI_INT, i, status.MPI_SOURCE, MPI_COMM_WORLD);
-			printf("Wysylam: rank=%d, zegar=%d, czyChceLicencje=%d\n", wiadomosc[0],wiadomosc[1],wiadomosc[2]);
+			printf("Wysylam: (ja: %d) do %d, zegar=%d\n", wiadomosc[0],i,wiadomosc[1]);
 		}
-		for(int i=0; i<size; i++) {
+
+		printf("JA:%d czekam na odpowiednia ilosc zgod licencji\n",rank);
+		while(ileMamZgodLicencja < Mysliwi - Licencje + 1) {
+			//oczekuj na odpowiednia ilosc zgod
+		}
+		printf("JA:%d Dostalem odpowiednia ilosc zgod: %d i moge wejsc do parku !!\n", rank, ileMamZgodLicencja);
+
+
+		/*for(int i=0; i<size; i++) {
 			if(i==rank) continue;
 			MPI_Recv(&wiadomosc, rozmiarWiadomosci, MPI_INT, MPI_ANY_SOURCE, tagWiadomosci, MPI_COMM_WORLD, &status);
 			printf("Odbieram: rank=%d, zegar=%d, czySieZgodzil=%d\n", wiadomosc[0],wiadomosc[1],wiadomosc[3]);
-		}
+		}*/
     }
+	else {
+		for(int i=0; i<1000000000;i++) {} //sleep
+	}
 
 
 	printf("wyswietlam w mainie\n");
@@ -136,7 +184,13 @@ void init() {
         zegarLamporta = rand() % (size);
 }
 
-void * funkcjaWatku1() {
+void aktualizujZegar() {
+	pthread_mutex_lock(&mutexZegar);
+    zegarLamporta += 1;
+    pthread_mutex_unlock(&mutexZegar);	
+}
+
+void * funkcjaWatku2() {
 	while(!end) {
 		MPI_Recv(&wiadomosc, rozmiarWiadomosci, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		printf("status:%d, Otrzymalem od procesu: %d, wiadomosc o zegarze[%d], coChce[%d]\n",status.MPI_SOURCE,wiadomosc[0],wiadomosc[1],wiadomosc[2]);
@@ -159,7 +213,7 @@ void * funkcjaWatku1() {
 			}
 			else {
 				wiadomosc[3] = 0;
-				iadomosc,rozmiarWiadomosci, MPI_INT, wiadomosc[0], status.MPI_SOURCE, MPI_COMM_WORLD);
+				//iadomosc,rozmiarWiadomosci, MPI_INT, wiadomosc[0], status.MPI_SOURCE, MPI_COMM_WORLD);
 			}
 			
 		}
